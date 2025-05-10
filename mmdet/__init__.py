8f70d5d6c06cf85b855a673dcd2ac64e176ad679
Nikita <nikitazag3@gmail.com>
2025-05-10 14:59:48 +0300

    Format __init__.py

--- mmdet/__init__.py
+++ mmdet/__init__.py
@@ -5,12 +5,12 @@ from mmengine.utils import digit_version
 
 from .version import __version__, version_info
 
-mmcv_minimum_version = '2.0.0rc4'
-mmcv_maximum_version = '2.2.0'
+mmcv_minimum_version = "2.0.0rc4"
+mmcv_maximum_version = "2.2.0"
 mmcv_version = digit_version(mmcv.__version__)
 
-mmengine_minimum_version = '0.7.1'
-mmengine_maximum_version = '1.0.0'
+mmengine_minimum_version = "0.7.1"
+mmengine_maximum_version = "1.0.0"
 mmengine_version = digit_version(mmengine.__version__)
 
 # assert (mmcv_version >= digit_version(mmcv_minimum_version)
@@ -24,4 +24,4 @@ mmengine_version = digit_version(mmengine.__version__)
 #     f'Please install mmengine>={mmengine_minimum_version}, ' \
 #     f'<{mmengine_maximum_version}.'
 
-__all__ = ['__version__', 'version_info', 'digit_version']
+__all__ = ["__version__", "version_info", "digit_version"]
